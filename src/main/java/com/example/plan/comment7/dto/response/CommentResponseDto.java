0e9f80de793aac4241e0bae084649ccd87d66df2
backswan0 <rnrwprh3434@gmail.com>
2024-12-31 20:08:59 +0900

    :wrench: refactor/jh-main-response dto
    refactor response DTOs to exclude unnecessary data in plan and comment creation

--- src/main/java/com/example/plan/comment7/dto/response/CommentResponseDto.java
+++ src/main/java/com/example/plan/comment7/dto/response/CommentResponseDto.java
@@ -1,21 +1,27 @@
 package com.example.plan.comment7.dto.response;
 
 import com.example.plan.comment7.entity.Comments;
-import com.example.plan.plan7.entity.Plan;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.time.LocalDateTime;
 
 public record CommentResponseDto(
         Long id,
+        Long planId,
         String content,
-        Plan plan
-) {
 
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+        LocalDateTime createdAt
+
+) {
     public static CommentResponseDto toDto(
             Comments comments
     ) {
         return new CommentResponseDto(
                 comments.getId(),
+                comments.getPlan().getId(),
                 comments.getContent(),
-                comments.getPlan()
+                comments.getCreatedAt()
         );
     }
 }
--- src/main/java/com/example/plan/plan7/dto/response/PlanResponseDto.java
+++ src/main/java/com/example/plan/plan7/dto/response/PlanResponseDto.java
@@ -1,6 +1,5 @@
 package com.example.plan.plan7.dto.response;
 
-import com.example.plan.member7.entity.Member;
 import com.example.plan.plan7.entity.Plan;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
@@ -8,25 +7,21 @@ import java.time.LocalDateTime;
 
 public record PlanResponseDto(
         Long id,
+        Long memberId,
         String title,
         String task,
-        Member member,
 
         @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-        LocalDateTime createdAt,
-
-        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-        LocalDateTime updatedAt
+        LocalDateTime createdAt
 ) {
 
     public static PlanResponseDto toDto(Plan plan) {
         return new PlanResponseDto(
-                plan.getId()
-                , plan.getTitle()
-                , plan.getTask()
-                , plan.getMember()
-                , plan.getCreatedAt()
-                , plan.getUpdatedAt()
+                plan.getId(),
+                plan.getMember().getId(),
+                plan.getTitle(),
+                plan.getTask(),
+                plan.getCreatedAt()
         );
     }
 }
